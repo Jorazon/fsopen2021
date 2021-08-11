7be9f95bb4ca1c4b06e00baa529102ebb26fe620
Jorazon <oskari.pahkala@gmail.com>
2021-08-11 17:41:43 +0300

    1.6

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,7 +6,25 @@ const App = () => {
 	const [neutral, setNeutral] = useState(0);
 	const [bad, setBad] = useState(0);
 
-	return <div>code here</div>;
+	return (
+		<>
+			<h2>give feedback</h2>
+
+			<button onClick={() => setGood(good + 1)}>good</button>
+			<button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+			<button onClick={() => setBad(bad + 1)}>bad</button>
+
+			<h2>statistics</h2>
+
+			<p>
+				good {good}
+				<br />
+				neutral {neutral}
+				<br />
+				bad {bad}
+			</p>
+		</>
+	);
 };
 
 export default App;
