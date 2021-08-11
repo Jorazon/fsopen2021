dfc40b050abafcaac398a0c4896faa20749f94ae
Jorazon <oskari.pahkala@gmail.com>
2021-08-11 18:59:57 +0300

    1.11

--- part1/unicafe/src/Statistics.js
+++ part1/unicafe/src/Statistics.js
@@ -9,14 +9,16 @@ const Statistics = ({ good, neutral, bad }) => {
 			{total === 0 ? (
 				<p>No feedback given</p>
 			) : (
-				<>
-					<StatisticLine text="good" value={good} />
-					<StatisticLine text="neutral" value={neutral} />
-					<StatisticLine text="bad" value={bad} />
-					<StatisticLine text="all" value={total} />
-					<StatisticLine text="average" value={(good - bad) / total} />
-					<StatisticLine text="positive" value={(good / total) * 100} />
-				</>
+				<table>
+					<tbody>
+						<StatisticLine text="good" value={good} />
+						<StatisticLine text="neutral" value={neutral} />
+						<StatisticLine text="bad" value={bad} />
+						<StatisticLine text="all" value={total} />
+						<StatisticLine text="average" value={(good - bad) / total} />
+						<StatisticLine text="positive" value={(good / total) * 100} />
+					</tbody>
+				</table>
 			)}
 		</>
 	);
