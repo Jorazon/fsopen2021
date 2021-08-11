a3c7238c93f64a76aba7539bbdc7eb6e81a7f607
Jorazon <oskari.pahkala@gmail.com>
2021-08-11 13:31:21 +0300

    1.3

---
+++ part1/course_information/src/Content.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Part from "./Part";
+
+const Content = () => {
+	return (
+		<>
+			<Part name={""} />
+			<Part name={""} />
+			<Part name={""} />
+		</>
+	);
+};
+
+export default Content;
---
+++ part1/course_information/src/Part.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Part = (props) => {
+	return <></>;
+};
+
+export default Part;
