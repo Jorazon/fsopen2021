12971a69e8a94cbe5d2938c4ce7a439d6272380a
Jorazon <oskari.pahkala@gmail.com>
2021-08-12 04:44:08 +0300

    1.5

---
+++ part2/course_information/src/Content.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Part from "./Part";
+
+const Content = (props) => {
+	return (
+		<>
+			{props.course.parts.map(({ name, exercises }, index) => (
+				<Part name={name} exercises={exercises} key={index} />
+			))}
+		</>
+	);
+};
+
+export default Content;
