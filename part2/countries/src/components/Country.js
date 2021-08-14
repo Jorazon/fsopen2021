2a24cb8bddcd209c6d81d53eac7a223a40bdadd0
Jorazon <oskari.pahkala@gmail.com>
2021-08-15 01:34:06 +0300

    2.14

--- part2/countries/src/components/Country.js
+++ part2/countries/src/components/Country.js
@@ -1,6 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import axios from "axios";
 
 const Country = ({ country }) => {
+	const [weather, setWeather] = useState(null);
+
+	const wind = [
+		"Undefined",
+		"Below 0.3m/s",
+		"0.3-3.4m/s",
+		"3.4-8.0m/s",
+		"8.0-10.8m/s",
+		"10.8-17.2m/s",
+		"17.2-24.5m/s",
+		"24.5-32.6m/s",
+		"Over 32.6m/s",
+	];
+
+	useEffect(() => {
+		setWeather(null);
+		const url = `http://www.7timer.info/bin/api.pl?lon=${country.latlng[1]}&lat=${country.latlng[0]}&output=json&product=civil`;
+		axios.get(url).then((response) => {
+			setWeather(response.data.dataseries[0]);
+		});
+	}, [country]);
+
+	console.log(weather);
+
 	return (
 		<>
 			<h2>{country.name}</h2>
@@ -8,13 +34,25 @@ const Country = ({ country }) => {
 			<br />
 			population {country.population}
 			<br />
-			<h3>languages</h3>
+			<h3>Spoken languages</h3>
 			<ul>
 				{country.languages.map((language) => {
 					return <li>{language.name}</li>;
 				})}
 			</ul>
 			<img src={country.flag} alt={`Flag of ${country.name}`} width={"100px"} />
+			{weather && (
+				<>
+					<h3>Weather in {country.capital}</h3>
+					<b>temperature: </b>
+					{weather.temp2m} Celcius
+					<br />
+					<b>wind: </b>
+					{wind[weather.wind10m.speed]}
+					{" direction "}
+					{weather.wind10m.direction}
+				</>
+			)}
 		</>
 	);
 };
