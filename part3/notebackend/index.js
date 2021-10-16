b2154f883af832bf97910ac8979be6efb019f279
Oskari Pahkala <oskari.pahkala@gmail.com>
2021-10-16 17:33:25 +0300

    frontend

--- part3/notebackend/index.js
+++ part3/notebackend/index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 var morgan = require("morgan");
 const app = express();
 app.use(cors());
+app.use(express.static("build"));
 app.use(express.json());
 app.use(morgan("tiny"));
 /*
@@ -93,7 +94,7 @@ const unknownEndpoint = (request, response) => {
 app.use(unknownEndpoint);
 
 //start server
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
 });
